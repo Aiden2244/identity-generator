750375d43327db87ed528d70aea55406f65283de
Aiden2244 <aidenm2244@gmail.com>
2024-02-21 02:35:50 -0500

    Restructured the project so that it can be run as a single executable.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt and store its contents in a list
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='idgen',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=requirements,  # Use the list created from requirements.txt
+    entry_points={
+        'console_scripts': [
+            'idgen=my_package.main:main',  # "idgen" command will call main() in my_package/main.py
+        ],
+    },
+)
